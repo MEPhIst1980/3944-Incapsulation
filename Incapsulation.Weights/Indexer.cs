5e3f8aaa84c192940e38bcbea3d09d33c6432381
mephist1980 <mephist1980@gmail.com>
2018-12-06 23:52:59 +0300

    раздел 3.3. Задача 3

--- Incapsulation.Weights/Indexer.cs
+++ Incapsulation.Weights/Indexer.cs
@@ -6,5 +6,71 @@ using System.Threading.Tasks;
 
 namespace Incapsulation.Weights
 {
-	
+    public class Indexer
+    {
+        private double[] data;
+        private int min = 0;
+        private int max = 0;
+
+        public int Length
+        {
+            get { return max; }
+        }
+
+        public Indexer(double[] array, int start, int length)
+        {
+            if (array == null) {
+                throw new ArgumentException();
+            }
+            if (array.Length == 0) {
+                throw new ArgumentException();
+            }
+            if (start < 0) {
+                throw new ArgumentException();
+            }
+            if (length <= 0) {
+                throw new ArgumentException();
+            }
+            if (array.Length < start) {
+                throw new ArgumentException();
+            }
+            if (array.Length < length)
+            {
+                throw new ArgumentException();
+            }
+            if (array.Length < start + length)
+            {
+                throw new ArgumentException();
+            }
+            this.data = array;
+            this.max = length;
+            this.min = start;
+        }
+
+        public double this[int index]
+        {
+            get {
+                if (index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                if (max < min + index)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return data[min + index];
+            }
+            set {
+                if (index < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                if (max < min + index)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                data[min + index] = value;
+            }
+        }
+    }
 }
